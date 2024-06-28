e2c13e18d0b7f10bba99bd0779af0517a4dde62f
damar <damar@gitlab.lab>
2024-06-28 10:48:54 +0700

    fix error build

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,67 @@
-import { AppHighlightContent } from "@/components/main/AppHighlightContent";
-import Image from "next/image";
+// import Head from 'next/head'
 import getPosts from '../helpers/getPosts';
-// import Head from "next/head";
 
-// export function getStaticProps(): any {
-//   const posts = getPosts();
+import { AppHighlightContent } from '../components/main/AppHighlightContent'
+// import PagesDirection from '../components/PagesDirection';
 
-//   return {
-//     props: {
-//       posts,
-//     },
+
+const getStaticProps = ({params}: any) => {
+    const pageSize = 5;
+    const posts = getPosts()
+    const { page } = params;
+    const slicedPosts = posts.slice(pageSize * (parseInt(page) - 1), (pageSize * parseInt(page)));
+    return {
+        posts: slicedPosts,
+        page,
+        pageSize,
+        pages: Math.ceil(posts.length / pageSize) 
+        
+    };
+};
+
+// export const getStaticPaths = async () => {
+//     const pageSize = 5;
+//     const posts = await getPosts();
+//     const allPages = Array.from({length: Math.ceil(posts.length / 3)}, (_, i) => i + 1);
+//     const paths = allPages.map((page) => ({ params: { page: page.toString() } }));
+//     return {
+//       paths,
+//       fallback: false,
+//     };
 //   };
-// };
 
-export default function Home() {
-  const posts = getPosts();
+export default function Home(props: any) {
+  const {
+    posts, page,
+    pageSize,
+    pages 
+    
+} = getStaticProps(props)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       {/* <Head>
         <title>ngeblog</title>
         <meta name="description" content="Mari ngeblog, blog" />
         <link rel="icon" href="/pixel_me_cropped.png" />
       </Head> */}
 
-      <h1 className="mt-24 mb-12 font-bold text-4xl md:text-7xl">Latest Post</h1>
+      
+      <h1 className="mt-24 mb-12 font-bold text-3xl md:text-7xl z-50">Lastest Post</h1>
       {posts.map((post: any) => (
-
-          <AppHighlightContent className="w-11/12"
+        <AppHighlightContent className="w-11/12"
           key={post.slug}
           title={post.data.title}
+          tags={post.data.tags}
           date={post.data.date}
           description={post.data.description}
           slug={post.slug}
-          />
+        />
       ))}
+      {/* <PagesDirection page={page} pages={pages} /> */}
+    
     </main>
-  );
+  )
 }
+
+
+  
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    imageSizes:  [500, 400, 300, 200 ,100, 96, 76],
     redirects: () => {
         return [{
         source: '/',
