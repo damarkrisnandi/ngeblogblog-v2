eb4578a1a388d7bccd39f2036fe96a7878287f58
Damar Krisnandi Ramdan <damarkrisnandi1202@gmail.com>
2024-06-29 22:33:38 +0700

    Update AppNewMark.tsx

--- components/main/AppNewMark.tsx
+++ components/main/AppNewMark.tsx
@@ -3,15 +3,15 @@ import { Badge } from "../ui/badge"
 import { useState, useEffect } from "react"
 
 export function AppNewMark({ date }: any) {
-    const [now, setNow] = useState(null);
+    const [now, setNow] = useState(0);
 
     useEffect(() => {
-        if (!now) {
-            setNow(new Date());
+        if (!now && now === 0) {
+            setNow(new Date().getTime());
         }
     })
     
-    if (now && now.getTime() <= new Date(date).getTime() + (1000 * 60 * 60 * 24)) {
+    if (now && now <= new Date(date).getTime() + (1000 * 60 * 60 * 24)) {
         return (
             <Badge variant={'outline'} className="border-red-600 text-red-600 font-bold"> NEW! </Badge>
         )
