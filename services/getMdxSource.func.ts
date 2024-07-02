5eddef5a7d3f19180ff93fa58c1b7f205afad46e
damar <damar@gitlab.lab>
2024-07-02 13:13:06 +0700

    use class any

--- services/getMdxSource.func.ts
+++ services/getMdxSource.func.ts
@@ -4,7 +4,7 @@ import { serialize } from "next-mdx-remote/serialize";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
-export function getMdxSource(post: PostModel) {
+export function getMdxSource(post: any) {
     return serialize(post.content, {
         // MDX's available options, see the MDX docs for more info.
         // https://mdxjs.com/packages/mdx/#compilefile-options
