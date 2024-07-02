d74c1ffac84ab17f5eaefcf720bd2301825e51db
damarkrisnandi1202 <damarkrisnandi1202@gmail.com>
2024-07-02 10:50:45 +0000

    add gfm and inserting component to mdx remote

--- components/main/AppMDXViewer.tsx
+++ components/main/AppMDXViewer.tsx
@@ -1,10 +1,26 @@
 'use client'
 import { MDXRemote } from "next-mdx-remote";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
+
+/**
+ * first, register all components needed here
+ */
+const components = { 
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle 
+}
+
 
 function AppMDXViewer(props: any) {
   return (
-    <div className="w-full">
-        <MDXRemote {...props} />
+    <div className="w-full wrap">
+        <MDXRemote {...props} components={components} />
     </div>
     );
 }
--- services/getMdxSource.func.ts
+++ services/getMdxSource.func.ts
@@ -3,13 +3,14 @@ import { PostModel } from '../models/post.model';
 import { serialize } from "next-mdx-remote/serialize";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import remarkGfm from 'remark-gfm';
 
 export function getMdxSource(post: any) {
     return serialize(post.content, {
         // MDX's available options, see the MDX docs for more info.
         // https://mdxjs.com/packages/mdx/#compilefile-options
         mdxOptions: {
-          remarkPlugins: [remarkMath],
+          remarkPlugins: [remarkMath, remarkGfm],
           rehypePlugins: [rehypeKatex],
           format: "mdx",
         },
